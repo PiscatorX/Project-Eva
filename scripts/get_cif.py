3ba2bbadf9d254ce2fd53d5dca8070b75978cabe
Andrew Ndhlovu <drewxdvst@outlook.com>
2022-03-09 13:31:59 +0200

    print to screen

--- scripts/get_cif.py
+++ scripts/get_cif.py
@@ -11,7 +11,7 @@ import os
 
 
 
-def scrape_website(total_entries, start_page = 1, page_limit = 200,  relpath = "hypotheticalzeolite", log = ".zeo_history"):
+def scrape_website(total_entries,  start_page = 1, page_limit = 200,  relpath = "hypotheticalzeolite", log = ".zeo_history"):
 
     logfile = os.path.join(relpath,log)
     if os.path.exists(logfile):
@@ -25,11 +25,11 @@ def scrape_website(total_entries, start_page = 1, page_limit = 200,  relpath = "
     for page in it.count(start=start_page):
         url  = url.format(page, page_limit)
         for link1 in get_elements(url, 'a', 'href'):
-            time.sleep(1)
             if "viewer" in link1:
                 follow = "".join([url_p.scheme+"://", url_p.netloc, link1])
                 cif_fname = urlparse(follow).query.split("=")[1]
                 if cif_fname in history:
+                    entry_counter += 1
                     continue
                 for link2 in get_elements(follow, 'a', None):
                     time.sleep(1)
@@ -38,9 +38,10 @@ def scrape_website(total_entries, start_page = 1, page_limit = 200,  relpath = "
                         cif_link =   "".join([url_p.scheme+"://", url_p.netloc, rel_soup.a['href']])
                         save2disk(cif_link, cif_fname, logfile, relpath)
                         entry_counter += 1
-                        if entry_counter == 5:
+                        print(entry_counter, cif_fname)
+                        if entry_counter == total_entries:
                             sys.exit(0)
-                        time.sleep(5)
+                        time.sleep(3)
                         
                         
     
@@ -67,14 +68,10 @@ def save2disk(cif_link, cif_fname, logfile, relpath):
     with open(cif_path, 'wb') as cif_fobj:
         cif_fobj.write(cif_data.read())
 
-    log_fobj = open(logfile, 'a+')
-    print(cif_path)
-    print(cif_fname, file=log_fobj)  
-
-
-
+    with open(logfile, 'a+') as log_fobj:
+        print(cif_fname, file=log_fobj)  
 
     
 if __name__  ==  "__main__":
-    scrape_website(total_entries =  5)    
+    scrape_website(start_page = 200, total_entries =  100000)    
     
