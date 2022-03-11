b3f3f75d32fc25f96fbd20a1f0039c295faa4f92
Andrew Ndhlovu <drewxdvst@outlook.com>
2022-03-11 18:35:37 +0200

    fix formating issue

--- scripts/constant.py
+++ scripts/constant.py
@@ -1,2 +1,2 @@
 
-CONFIRMED_DATA_URL = "http://www.hypotheticalzeolites.net./dataconfirmed?page={}&ent_pp={}&orderby=bgbener&toggle=-asc"
+CONFIRMED_DATA_URL = "http://www.hypotheticalzeolites.net./dataconfirmed?page={page}&ent_pp={pp}&orderby=bgbener&toggle=-asc"
